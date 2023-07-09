03ddee0fd11947921a9f81f73ce1be6add048180
Alex Filimonov <filimonovalexander9997@gmail.com>
2023-07-09 10:37:00 +0300

    complete task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,7 @@ import css from './App.module.css';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -51,6 +46,16 @@ export class App extends Component {
     );
     this.setState({ contacts: filter });
   };
+
+  componentDidMount() {
+    const items = localStorage.getItem('contacts');
+    this.setState({ contacts: JSON.parse(items) });
+  }
+
+  componentDidUpdate() {
+    const { contacts } = this.state;
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
 
   render() {
     const {
